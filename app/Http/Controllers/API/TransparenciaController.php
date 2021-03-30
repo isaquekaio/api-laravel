fb9810135a480685f32d0cd203a14f4f8e35e5df
Isaque Kaio <isaque.araujo@escolar.ifrn.edu.br>
2021-03-29 22:08:38 -0300

    Ajuste

--- app/Http/Controllers/API/TransparenciaController.php
+++ app/Http/Controllers/API/TransparenciaController.php
@@ -40,63 +40,65 @@ class TransparenciaController extends Controller
         //intval($faixa);
 
         $pacientes_qtd = DB::table('pacientes')
-            ->when($doenca != null, function ($query){
+            ->when($doenca != null, function ($query) use ($doenca){
                 return $query
-                    ->crossJoin('doencas_tem_pacientes')
-                    ->orderByDesc('doenca_id');
+                    ->join('doencas_tem_pacientes', 'pacientes.id', '=', 'doencas_tem_pacientes.paciente_id')
+                    ->where('doencas_tem_pacientes.doenca_id', '=', $doenca);
             })->when($uf != null, function ($query) use ($uf){
-                return $query->where('uf_id', $uf)->orderByDesc('uf_id');
+                return $query->where('uf_id', $uf);
             })->when($municipio != null, function ($query) use ($municipio){
-                return $query->where('municipio_id', $municipio)->orderByDesc('municipio_id');
+                return $query->where('municipio_id', $municipio);
             })->when($sexo != null, function ($query) use ($sexo){
-                return $query->where('sexo_id', $sexo)->orderByDesc('sexo_id');
+                return $query->where('sexo_id', $sexo);
             })->when($faixa != null, function ($query) use ($faixa){
                 $hoje = date("Y");
                 
                 $ano_nascimento = $query->pluck('data_nascimento')->map(function ($data) {
                     return $data->format('Y'); 
                 });
-
+                //dd(intval($hoje) - intval($ano_nascimento[1]));
+                //dd(date('Y',strtotime($ano_nascimento[0])));
+                //dd(intval($hoje) - intval(date('Y',strtotime($ano_nascimento[0]))));
                 for ($i = 0; $i < $ano_nascimento->count(); $i++){
 
-                    $idade = $hoje - $ano_nascimento[$i];
+                    $idade = intval($hoje) - intval(date('Y',strtotime($ano_nascimento[$i])));
+                
                     if ($faixa == 1 && ($idade >= 0 && $idade <= 18))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 2 && ($idade >= 19 && $idade <= 25))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 3 && ($idade >= 26 && $idade <= 35))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 4 && ($idade >= 36 && $idade <= 50))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 5 && ($idade >= 51 && $idade <= 60))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 6 && ($idade >= 60 && $idade <= 70))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 7 && ($idade >= 70 && $idade <= 80))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }else if ($faixa == 8 && ($idade >= 80 && $idade <= 100))
                     {
-                        return $query->orderByDesc('data_nascimento');
+                        return $query;
                         break;
                     }
                 }
             })->count();
     
-        
         return response([
             'pacientes_qtd' => $pacientes_qtd
         ], 200);
